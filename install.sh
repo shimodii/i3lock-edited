59e7fe05205e0b8d4edb001faa3c06d826f5fc51
Amir Mohammad <53581892+am-shm@users.noreply.github.com>
2020-08-20 19:09:33 +0430

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,48 @@
+apt update -y
+clear
+apt install figlet -y
+clear
+figlet "i3lock edited : )"
+echo "\n"
+echo "please wait until done massege"
+sleep 5
+apt install i3lock -y
+mkdir ~/.screenshot
+cp lock /usr/local/bin/
+clear
+clear
+figlet "done"
+sleep 0.5
+clear
+sleep 0.5
+figlet "done"
+sleep 0.5
+clear
+sleep 0.5
+figlet "done"
+sleep 0.1
+clear
+sleep 0.5
+figlet "done"
+sleep 0.5
+clear
+sleep 0.5
+figlet "done"
+sleep 0.5
+clear
+sleep 0.5
+figlet "done"
+sleep 0.5
+clear
+sleep 0.5
+figlet "done"
+sleep 0.5
+clear
+sleep 0.5
+figlet "done"
+sleep 0.5
+clear
+sleep 0.5
+figlet "done"
+sleep 1
+exit
