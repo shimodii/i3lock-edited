f1977636cb9586e0c7f44ddc316bb084b5eb003d
Amir Mohammad <53581892+am-shm@users.noreply.github.com>
2021-07-30 08:35:38 -0400

    Add files via upload

---
+++ debian-based-installtion.sh
@@ -0,0 +1,11 @@
+apt update -y
+clear
+echo "i3lock edited : )"
+echo "\n"
+echo "please wait until done massege"
+apt install i3lock -y
+mkdir ~/.screenshot
+cp lock /usr/local/bin/
+clear
+echo "done"
+exit
